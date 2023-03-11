0dfaf2beac519172103e39f35ab59dc6b90fdbbd
项项 惠 <sm200@126.com>
2023-03-12 06:49:53 +0800

    Propertychanged

--- ViewModelBase.cs
+++ ViewModelBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,10 @@ namespace WpfBeckhoff2023_3_11
     internal class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnporpertyChanged([CallerMemberName]string propertyName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
